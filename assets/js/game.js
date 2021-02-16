5e0775f16c55274ae90fd3cba06a3f535c786190
Senait <senutekie77@gmail.com>
2021-02-16 09:42:43 -0800

    initial game update

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,41 @@
-// this created a function namerd "fight"
-function fight() {
-    window.alert("The fight has begun!")
+var playerName = window.prompt("What is yuour robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// you can also log multiple value at once like this
+console.log(playerName, playerAttack, playerHealth)
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight =function() {
+    window.alert("Welcome to Robot Gladiators!");
+    
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+ enemyHealth = ememyHealth - playerAttack;
+
+ // Log a resulting message to the console so we know that it worked.
+ console.log(
+     playerName + "attaked " + enemyName + "." + enemyName + "has now" + enemyHealth + " health remaining."
+ )
+ //Tony the Robot attacked Roborto . Roborto has now 40 health remaing.
+
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+playerHealth = playerHealth - enemyAttack
+  // Log a resulting message to the console so we know that it worked.
+  console.log(
+      enemyName + "attacked" + playerName + "." + plyerNmae + "now has" + playerHealth + "health remaining."
+  )
+};
+
+fight();
+// check enemy's haelth
+if (enemyHealth <= 0) {
+   window.alert(enemyName + " has died!");
+  
 }
-// fight();
-var playerName = "Tony the robot";
-var playerName = window.prompt("What is your robot's name?");
-// what is this?
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-//this will do math and log 20
-console.log(10+10)
-// what is this?
-console.log("Out robot's name is " + playerName);
+else {
+  window.alert(enemyName + " still has ") + enemyHealth + "health left."
+}
 
